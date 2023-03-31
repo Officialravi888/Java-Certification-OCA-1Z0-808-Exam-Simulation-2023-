a121abf204e294651312231777d45d06306160dd
Officialravi888 <youremail@yourdomain.com>
2023-03-31 16:29:16 +0530

    exe17

---
+++ src/main/java/org/example/exe17/Test.java
@@ -0,0 +1,9 @@
+package org.example.exe17;
+
+public class Test {
+    public static void main(String[] args) {
+        String s1="Ravi";
+        String s2="Ravi"+"";
+        System.out.println(s1==s2);
+    }
+}
