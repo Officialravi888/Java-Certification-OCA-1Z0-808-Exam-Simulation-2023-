b424dd979f12e072462cefb283350fed7ecdaf3a
Officialravi888 <youremail@yourdomain.com>
2023-03-31 15:53:52 +0530

    exe13

---
+++ src/main/java/org/example/exe13/Test.java
@@ -0,0 +1,10 @@
+package org.example.exe13;
+
+public class Test {
+    public static void main(String[] args) {
+        short[] arg =new short[]{50,50};///compile arr // esme args nahi lek sakty
+        arg[0]=5;
+        arg[1]=10;
+        System.out.println("["+arg[0]+","+arg[1]+"]");
+    }
+}
