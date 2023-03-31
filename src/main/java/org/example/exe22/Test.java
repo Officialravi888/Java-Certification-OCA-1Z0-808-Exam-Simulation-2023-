1cb3df75fdc0a4186e8284477e386fd5087ee15a
Officialravi888 <youremail@yourdomain.com>
2023-03-31 16:51:51 +0530

    exe22

---
+++ src/main/java/org/example/exe22/Test.java
@@ -0,0 +1,8 @@
+package org.example.exe22;
+
+public class Test {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder=new StringBuilder("RAVI");
+        System.out.println(stringBuilder.length());
+    }
+}
