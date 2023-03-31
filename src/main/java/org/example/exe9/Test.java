662af7e97ef11228ce8c4e0c3f1b5edd04712b1a
Officialravi888 <youremail@yourdomain.com>
2023-03-31 14:58:42 +0530

    exe9

---
+++ src/main/java/org/example/exe9/Test.java
@@ -0,0 +1,30 @@
+package org.example.exe9;
+class SpecialString{
+    String str;
+
+    public SpecialString(String str) {
+        this.str = str;
+    }
+}
+public class Test {
+    public static void main(String[] args) {
+        Object [] arr=new Object[4];
+        for (int i=1; i<=3; i++){
+            switch (i){
+                case 1:
+                     arr[i]=new String("Java");
+                break;
+                case 2:
+                    arr[i]=new StringBuilder("Java");
+                    break;
+                case 3:
+                    arr[i]=new SpecialString("Java");
+                    break;
+            }
+        }
+        for (Object obj:arr){
+            System.out.println(obj);
+        }
+
+    }
+}
