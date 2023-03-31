3d5ed27387eed67a2eb02ac24308fbf5402c2f7f
Officialravi888 <youremail@yourdomain.com>
2023-03-31 15:14:24 +0530

    exe11

---
+++ src/main/java/org/example/exe11/Test.java
@@ -0,0 +1,8 @@
+package org.example.exe11;
+
+public class Test {
+    public static void main(String[] args) {
+        StringBuilder str=new StringBuilder(100);
+        System.out.println(str.length()+":"+str.toString().length());
+    }
+}
