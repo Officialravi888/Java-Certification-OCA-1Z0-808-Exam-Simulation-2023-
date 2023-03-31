f337583a53e87e797e88420ef5db1fba92bc5299
Officialravi888 <youremail@yourdomain.com>
2023-03-31 16:36:49 +0530

    exe19

---
+++ src/main/java/org/example/exe19/Test.java
@@ -0,0 +1,8 @@
+package org.example.exe19;
+
+public class Test {
+    public static void main(String[] args) {
+      //  System.out.println("Output is: " + 10 != 5); //compile time error
+        System.out.println(":"+5);
+    }
+}
