7d20bb5c9ae7b1033853ea0d18e0c714bf673ae6
Officialravi888 <youremail@yourdomain.com>
2023-03-31 13:18:49 +0530

    example1

---
+++ src/main/java/org/example/example1/A.java
@@ -0,0 +1,9 @@
+package org.example.example1;
+
+public class A {
+    public void print() {
+        System.out.println("A");
+    }
+}
+
+
---
+++ src/main/java/org/example/example1/Test1.java
@@ -0,0 +1,9 @@
+package org.example.example1;
+
+public class Test1 {
+    public static void main(String[] args) {
+        A obj = new A();
+        B obj2 = new B();
+        obj2.print();
+    }
+}
