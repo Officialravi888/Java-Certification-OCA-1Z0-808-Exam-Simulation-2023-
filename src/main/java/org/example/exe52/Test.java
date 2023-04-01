4d4a61af6936eab5652ef9fc4b84451cd2697ecd
Officialravi888 <youremail@yourdomain.com>
2023-04-01 15:33:47 +0530

    exe53

--- src/main/java/org/example/exe52/Test.java
+++ src/main/java/org/example/exe52/Test.java
@@ -1,2 +1,8 @@
-package org.example.exe52;public class Test {
+package org.example.exe52;
+
+public class Test {
+    public static void main(String [] args) {
+        int a = 100;
+        System.out.println(-a++);
+    }
 }
---
+++ src/main/java/org/example/exe53/Test.java
@@ -0,0 +1,10 @@
+package org.example.exe53;
+
+import java.time.Period;
+
+public class Test {
+    public static void main(String[] args) {
+        Period period = Period.of(0, 0, 0);
+        System.out.println(period);
+    }
+}
