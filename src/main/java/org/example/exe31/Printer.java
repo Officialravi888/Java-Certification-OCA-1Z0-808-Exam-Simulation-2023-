ea28d161c2406bfe27ef0c6959c129248c7b74dc
Officialravi888 <youremail@yourdomain.com>
2023-04-01 13:34:14 +0530

    exe31

---
+++ src/main/java/org/example/exe31/Printer.java
@@ -0,0 +1,6 @@
+package org.example.exe31;
+
+public class Printer {
+
+}
+//Which of the following correctly defines class Printer?
---
+++ src/main/java/org/example/exe31/Test.java
@@ -0,0 +1,7 @@
+package org.example.exe31;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
