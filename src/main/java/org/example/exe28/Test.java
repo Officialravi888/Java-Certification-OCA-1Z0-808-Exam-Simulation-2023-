4e6a7cb7d5551c546a331d6c2fd7cbd6b6b29156
Officialravi888 <youremail@yourdomain.com>
2023-04-01 13:22:42 +0530

    exe29

--- src/main/java/org/example/exe28/Test.java
+++ src/main/java/org/example/exe28/Test.java
@@ -3,4 +3,4 @@ package org.example.exe28;
 public class Test {
 }
     //Which of the following is not a valid array declaration?
-// int arr[]=new int[8]
+ // int arr[]=new int[8]
---
+++ src/main/java/org/example/exe29/Test.java
@@ -0,0 +1,8 @@
+package org.example.exe29;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    List<String> list=new ArrayList<String>();
+}
