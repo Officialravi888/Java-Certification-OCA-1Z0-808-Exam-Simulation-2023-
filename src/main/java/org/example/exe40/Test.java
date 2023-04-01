9d5f2004c0be06f078b2f88a8a9b3ea94d6d3bf7
Officialravi888 <youremail@yourdomain.com>
2023-04-01 14:32:59 +0530

    exe40

---
+++ src/main/java/org/example/exe40/Test.java
@@ -0,0 +1,11 @@
+package org.example.exe40;
+
+public class Test {
+    public static void main(String[] args) {
+        short [] arr;
+        arr = new short[3];
+        arr[1]=5;
+        arr[2]=10;
+        System.out.println("["+arr[1]+","+arr[2]+"]");
+    }
+}
