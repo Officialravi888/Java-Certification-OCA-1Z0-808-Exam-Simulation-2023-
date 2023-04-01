8639b2042d1550c5b050e821ac4aa6359935ffcf
Officialravi888 <youremail@yourdomain.com>
2023-04-01 13:05:46 +0530

    exe26

---
+++ src/main/java/org/example/exe26/Test.java
@@ -0,0 +1,8 @@
+package org.example.exe26;
+
+public class Test {
+    public static void main(String[] args) {
+      //  double []arr=new int[2];// compile erroe
+      //   System.out.println(arr[0]);
+    }
+}
