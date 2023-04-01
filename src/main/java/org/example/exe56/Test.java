dc3847b7506db72bdb97f7c4cf14d920a7bf0ee7
Officialravi888 <youremail@yourdomain.com>
2023-04-01 15:44:15 +0530

    exe56

---
+++ src/main/java/org/example/exe56/Test.java
@@ -0,0 +1,12 @@
+package org.example.exe56;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class Test {
+    public static void main(String [] args) {
+        LocalDateTime obj = LocalDateTime.now();
+        System.out.println(obj.getSecond());
+    }
+
+}
