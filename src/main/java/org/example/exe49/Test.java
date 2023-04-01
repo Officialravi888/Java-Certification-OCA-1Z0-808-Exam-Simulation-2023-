0b7d50a465417fef9f409a7290dbb8b967a757ba
Officialravi888 <youremail@yourdomain.com>
2023-04-01 15:23:06 +0530

    exe49

---
+++ src/main/java/org/example/exe49/Test.java
@@ -0,0 +1,10 @@
+package org.example.exe49;
+
+import java.time.LocalDate;
+
+public class Test {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.of(2020, 9, 31);
+        System.out.println(date);
+    }
+}
