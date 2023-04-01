c4d99484e0bcc907e1d09de53208e49700c71fc3
Officialravi888 <youremail@yourdomain.com>
2023-04-01 18:14:23 +0530

    exe4

---
+++ src/main/java/session2/exe4/Student.java
@@ -0,0 +1,28 @@
+package session2.exe4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+    private String name;
+    private int age;
+    Student(String name, int age){
+        this.name=name;
+        this.age=age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student["+name+","+age+"]";
+    }
+    public boolean equals(Object obj) {
+        if(obj instanceof Student) {
+            Student stud = (Student)obj;
+            if(this.name.equals(stud.name) && this.age == stud.age) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
