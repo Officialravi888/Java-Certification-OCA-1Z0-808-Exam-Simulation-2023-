a0b01fa5fe9ee8b0027a95b83377f6fd2cbbcd0a
Officialravi888 <youremail@yourdomain.com>
2023-04-01 15:20:44 +0530

    exe48

---
+++ src/main/java/org/example/exe48/Test.java
@@ -0,0 +1,12 @@
+package org.example.exe48;
+
+public class Test {
+    public static void main(String[] args) {
+        int grade = 60;
+       // if(grade = 60)///coimple error
+            if(grade != 60)
+            System.out.println("You passed...");
+        else
+            System.out.println("You failed...");
+    }
+}
