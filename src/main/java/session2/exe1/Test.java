c04facbff0ba870630f88dd376ffc850e165ba48
Officialravi888 <youremail@yourdomain.com>
2023-04-01 18:04:20 +0530

    exe1

--- src/main/java/session2/exe1/Test.java
+++ src/main/java/session2/exe1/Test.java
@@ -1,4 +1,14 @@
 package session2.exe1;
 
 public class Test {
+    private static void add(double d1, double d2){
+        System.out.println("doble version"+(d1+d2));
+    }
+    private static void add(Double d1, Double d2){
+        System.out.println("Double version"+(d1+d2));
+    }
+
+    public static void main(String[] args) {
+        add(10.0,new Integer(10));
+    }
 }
