854a41c41df9ec24c0fc052402048c035cd1bbd3
Officialravi888 <youremail@yourdomain.com>
2023-04-01 15:51:18 +0530

    exe59

---
+++ src/main/java/org/example/exe59/Test.java
@@ -0,0 +1,11 @@
+package org.example.exe59;
+
+public class Test {
+    public static void main(String[] args) {
+      //  m1(); error compilication
+        }
+
+    private static void m1() throws Exception { //Line 6
+        System.out.println("NOT THROWING ANY EXCEPTION"); //Line 7
+    }
+}
