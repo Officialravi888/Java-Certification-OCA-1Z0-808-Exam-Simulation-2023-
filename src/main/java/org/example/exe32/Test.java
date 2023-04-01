9dd9e5098d4559583df1733d39e214f61c4d0a49
Officialravi888 <youremail@yourdomain.com>
2023-04-01 13:37:08 +0530

    exe32

---
+++ src/main/java/org/example/exe32/Test.java
@@ -0,0 +1,7 @@
+package org.example.exe32;
+
+public class Test {
+}
+//Fill in the blanks for the definition of java.lang.Error class:
+//
+//public class java.lang.Error extends ________________ {...}
