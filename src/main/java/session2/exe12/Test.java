487cff0b661ba8aac6d8815bc7e6e8791e676034
Officialravi888 <youremail@yourdomain.com>
2023-04-02 17:52:06 +0530

    exe12

---
+++ src/main/java/session2/exe12/Test.java
@@ -0,0 +1,8 @@
+package session2.exe12;
+
+public class Test {
+    public static void main(String[] args) {
+        Vehicle obj = new Vehicle();
+        System.out.println(obj.getRegistrationNumber());
+    }
+}
---
+++ src/main/java/session2/exe12/Vehicle.java
@@ -0,0 +1,13 @@
+package session2.exe12;
+
+public class Vehicle {
+    public int getRegistrationNumber() {
+        return 1;
+    }
+}
+class Car {
+    public int getRegistrationNumber() {
+        return 2;
+    }
+}
+
