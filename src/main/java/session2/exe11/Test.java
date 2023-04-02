a7207bf4f3595befc1cc447d3d39dd5094ab3068
Officialravi888 <youremail@yourdomain.com>
2023-04-02 17:48:52 +0530

    exe11

---
+++ src/main/java/session2/exe11/Test.java
@@ -0,0 +1,18 @@
+package session2.exe11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        ArrayList<Integer> original = new ArrayList<>();
+        original.add(new Integer(10));
+
+        ArrayList<Integer> cloned = (ArrayList<Integer>) original.clone();
+        Integer i1 = cloned.get(0);
+        ++i1;
+
+        System.out.println(cloned);
+
+    }
+}
