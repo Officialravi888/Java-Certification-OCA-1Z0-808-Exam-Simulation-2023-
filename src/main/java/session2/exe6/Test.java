ab061a31cbf75114e966fa7ac2eb0d733e3172f2
Officialravi888 <youremail@yourdomain.com>
2023-04-02 17:17:39 +0530

    exe6

---
+++ src/main/java/session2/exe6/Test.java
@@ -0,0 +1,13 @@
+package session2.exe6;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List<String> list=new ArrayList<>();
+        list.add(0,"Array");
+        list.add(0,"List");
+        System.out.println(list);
+    }
+}
