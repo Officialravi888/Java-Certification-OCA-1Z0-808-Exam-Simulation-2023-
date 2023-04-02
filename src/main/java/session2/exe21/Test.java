7abf53c6ea0a00821ea4c9293a4faa7facf09a64
Officialravi888 <youremail@yourdomain.com>
2023-04-02 18:37:12 +0530

    exe21

---
+++ src/main/java/session2/exe21/Test.java
@@ -0,0 +1,10 @@
+package session2.exe21;
+
+public class Test {
+    public static boolean flage=!true;
+
+    public static void main(String[] args) {
+        System.out.println(!flage ? args[0]:args[1]);
+    }
+}
+//Run time error
