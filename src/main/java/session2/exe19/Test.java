54acfce996710c80f17a7fc7eb9f322984034db4
Officialravi888 <youremail@yourdomain.com>
2023-04-02 18:31:02 +0530

    exe19

---
+++ src/main/java/session2/exe19/Test.java
@@ -0,0 +1,10 @@
+package session2.exe19;
+
+public class Test {
+    public static void main(String[] args) {
+        int [] arr = {0, 2, 1};
+        for(int i : arr) {
+            System.out.println(arr[i]);
+        }
+    }
+}
