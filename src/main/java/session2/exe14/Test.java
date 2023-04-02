b7257f88da6979bc9ee01ecdb2505083353000c4
Officialravi888 <youremail@yourdomain.com>
2023-04-02 18:03:24 +0530

    exe14

---
+++ src/main/java/session2/exe14/Test.java
@@ -0,0 +1,20 @@
+package session2.exe14;
+
+import java.util.function.Predicate;
+
+public class Test {
+    public static void main(String[] args) {
+        String [] arr = {"A", "ab", "bab", "Aa", "bb", "baba", "aba", "Abab"};
+
+        processStringArray(arr,p->true);
+    }
+
+    private static void processStringArray(String [] arr,
+                                           Predicate<String> predicate) {
+        for(String str : arr) {
+            if(predicate.test(str)) {
+                System.out.println(str);
+            }
+        }
+    }
+}
