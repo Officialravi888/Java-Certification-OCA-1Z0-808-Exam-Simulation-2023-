19c8d71684dc564c45ec9c196a2294590f8d6621
Officialravi888 <youremail@yourdomain.com>
2023-04-02 18:21:00 +0530

    exe17

---
+++ src/main/java/session2/exe17/Helper.java
@@ -0,0 +1,9 @@
+package session2.exe17;
+
+public class Helper {
+    int num=100;
+    String operation=null;
+    void log() {
+        System.out.println("Helper-log");
+    }
+}
---
+++ src/main/java/session2/exe17/Loghelper.java
@@ -0,0 +1,17 @@
+//package session2.exe17;
+//
+//public class Loghelper extends Helper {
+//    private int num = 200;
+//    protected String operation="LOGING";
+//    void help(){
+//        System.out.println("LogHelper-help");
+//    }
+//    void help(){
+//        System.out.println("LogHelper-log");
+//    }
+//
+//    public static void main(String[] args) {
+//        new Loghelper().help();
+//    }
+//
+//}
