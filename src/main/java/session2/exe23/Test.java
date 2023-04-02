e12fef164dddc736acaf318070f069ddf6e945f1
Officialravi888 <youremail@yourdomain.com>
2023-04-02 18:43:38 +0530

    exe23

---
+++ src/main/java/session2/exe23/Test.java
@@ -0,0 +1,12 @@
+package session2.exe23;
+public class Test {
+    public static void main(String[] args) {
+        int arr[]={0,1,2,3,4,5,6,7,8,9};
+        String str = process(arr, 3, 8); //Line 5
+        System.out.println(str);
+    }
+    private static String process(int[]arr,int start,int end){
+        return null;
+    }
+    /*INSERT*/
+}
