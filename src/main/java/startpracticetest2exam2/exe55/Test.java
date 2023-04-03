1a59d2df8a1a5d303ea48f840ad44298f85647f5
Officialravi888 <youremail@yourdomain.com>
2023-04-03 12:42:22 +0530

    exe55

---
+++ src/main/java/startpracticetest2exam2/exe55/Test.java
@@ -0,0 +1,20 @@
+package startpracticetest2exam2.exe55;
+
+import java.util.function.Predicate;
+
+public class Test {
+        public static void main(String[] args) {
+            String [] arr = {"A", "ab", "bab", "Aa", "bb", "baba", "aba", "Abab"};
+
+            processStringArray(arr, p->true);
+        }
+
+        private static void processStringArray(String [] arr,
+                                               Predicate<String> predicate) {
+            for(String str : arr) {
+                if(predicate.test(str)) {
+                    System.out.println(str);
+                }
+            }
+        }
+}
