c342163a7a0cab89923d85be97090ddfcfa61fd5
Officialravi888 <youremail@yourdomain.com>
2023-04-03 12:04:25 +0530

    exe42

--- src/main/java/startpracticetest2exam2/exe41/Test.java
+++ src/main/java/startpracticetest2exam2/exe41/Test.java
@@ -1,21 +1,22 @@
-//package startpracticetest2exam2.exe41;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class Test {
-//    public static void main(String[] args) {
-//        List<String> list = new ArrayList<>();
-//        list.add("ONE");
-//        list.add("TWO");
-//        list.add("THREE");
-//        list.add("THREE");
-//
-//        if (list.remove(2)) {
-//            list.remove("THREE");
-//        }
-//
-//        System.out.println(list);
-//    }
-//}
-//
+package startpracticetest2exam2.exe41;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("ONE");
+        list.add("TWO");
+        list.add("THREE");
+        list.add("THREE");
+
+        if (list.remove("THREE")) {
+            list.remove("THREE");
+
+        }
+
+        System.out.println(list);
+    }
+}
+
