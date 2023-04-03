e8765542531d99774ab7ca5ba72528bb6d25924a
Officialravi888 <youremail@yourdomain.com>
2023-04-03 11:15:02 +0530

    exe25

---
+++ src/main/java/startpracticetest2exam2/exe25/Test.java
@@ -0,0 +1,9 @@
+package startpracticetest2exam2.exe25;
+
+public class Test {
+    private static boolean flag = !true;
+
+    public static void main(String [] args) {
+        System.out.println(!flag ? args[0] : args[1]);
+    }
+}
