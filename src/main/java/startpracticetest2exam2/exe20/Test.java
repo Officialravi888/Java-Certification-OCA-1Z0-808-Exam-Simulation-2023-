270a31b6bfb411ec5b0f81488c61daf35207ed4b
Officialravi888 <youremail@yourdomain.com>
2023-04-03 11:04:34 +0530

    exe20

---
+++ src/main/java/startpracticetest2exam2/exe20/Test.java
@@ -0,0 +1,15 @@
+package startpracticetest2exam2.exe20;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add(0, "Array");
+        list.add(0, "List");
+
+        System.out.println(list);
+    }
+
+}
---
+++ src/main/java/startpracticetest2exam2/exe21/Test.java
@@ -0,0 +1,18 @@
+package startpracticetest2exam2.exe21;
+
+public class Test {
+    public static void main(String[] args) {
+        String fruit="mango";
+        switch (fruit) {
+            case "Apple":
+                System.out.println("Apple");
+            case "Mango":
+                    System.out.println("MANGO");
+            case "Banana":
+                System.out.println("BANANA");
+                break;
+            default:
+                System.out.println("ANY FRUIT WILL DO");
+        }
+    }
+}
