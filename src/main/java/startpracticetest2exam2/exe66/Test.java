c35e89e50a186aeca89a79cbcbff4528fe8a321f
Officialravi888 <youremail@yourdomain.com>
2023-04-03 12:54:04 +0530

    exe66

---
+++ src/main/java/startpracticetest2exam2/exe66/Test.java
@@ -0,0 +1,11 @@
+package startpracticetest2exam2.exe66;
+
+public class Test {
+    public static void main(String[] args) {
+        Boolean b1 = new Boolean("tRuE");
+        Boolean b2 = new Boolean("fAlSe");
+        Boolean b3 = new Boolean("abc");
+        Boolean b4 = null;
+        System.out.println(b1 + ":" + b2 + ":" + b3 + ":" + b4);
+    }
+}
