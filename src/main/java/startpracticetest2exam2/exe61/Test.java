9f8d4fe790f603ce042adfd8a8931cb07dc2a6cb
Officialravi888 <youremail@yourdomain.com>
2023-04-03 12:49:11 +0530

    exe61

---
+++ src/main/java/startpracticetest2exam2/exe61/Test.java
@@ -0,0 +1,9 @@
+package startpracticetest2exam2.exe61;
+
+public class Test {
+    public static void main(String[] args) {
+        int [] arr = {3, 2, 1};
+        for(int i : arr) {
+            System.out.println(arr[i]);
+        }
+}}
