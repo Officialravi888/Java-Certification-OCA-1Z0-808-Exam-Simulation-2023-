ece5ca17f6551e57be76a09e83c0ae20a44bbb7e
Officialravi888 <youremail@yourdomain.com>
2023-04-03 12:52:44 +0530

    exe64

---
+++ src/main/java/startpracticetest2exam2/exe65/Test.java
@@ -0,0 +1,11 @@
+package startpracticetest2exam2.exe65;
+
+import java.time.LocalDate;
+
+public class Test {
+        public static void main(String [] args) {
+            LocalDate date = LocalDate.parse("2018-1-01");
+            System.out.println(date);
+        }
+    }
+}
