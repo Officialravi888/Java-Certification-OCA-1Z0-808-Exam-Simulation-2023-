be284d7a2a2e20446cc4dd5f5f8e0202a55cc7d0
Officialravi888 <youremail@yourdomain.com>
2023-04-03 11:43:29 +0530

    exe36

---
+++ src/main/java/startpracticetest2exam2/exe36/Test.java
@@ -0,0 +1,12 @@
+package startpracticetest2exam2.exe36;
+
+public class Test {
+    public static void main(String[] args) {
+        String str1 = " ";
+        boolean b1 = str1.isEmpty();
+        System.out.println(b1);
+        str1.trim();
+        b1 = str1.isEmpty();
+        System.out.println(b1);
+    }
+}
