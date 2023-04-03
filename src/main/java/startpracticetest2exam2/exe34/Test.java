0b676a88e6a6155a76037aa8764c214e6383f06f
Officialravi888 <youremail@yourdomain.com>
2023-04-03 11:40:08 +0530

    exe34

---
+++ src/main/java/startpracticetest2exam2/exe34/Test.java
@@ -0,0 +1,11 @@
+package startpracticetest2exam2.exe34;
+class A{}
+class B extends A{}
+public class Test {
+  //  Which is not a valid statement based on given code?
+  public static void main(String[] args) {
+      A a=new B();
+      System.out.println("a");
+  }
+
+}
