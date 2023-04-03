3199ed471961b23974e055d65b65d38f17c6950c
Officialravi888 <youremail@yourdomain.com>
2023-04-03 10:56:52 +0530

    exe17

---
+++ src/main/java/startpracticetest2exam2/exe17/Test.java
@@ -0,0 +1,10 @@
+package startpracticetest2exam2.exe17;
+
+import java.time.Period;
+
+public class Test {
+    public static void main(String [] args) {
+        Period period = Period.of(2, 1, 0).ofYears(10).ofMonths(5).ofDays(2);
+        System.out.println(period);
+    }
+}
