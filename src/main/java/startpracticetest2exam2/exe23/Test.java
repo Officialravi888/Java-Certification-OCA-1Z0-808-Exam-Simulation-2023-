5f7a02b1f202085366ce64df5ec50856f50872e6
Officialravi888 <youremail@yourdomain.com>
2023-04-03 11:08:59 +0530

    exe23

---
+++ src/main/java/startpracticetest2exam2/exe23/Test.java
@@ -0,0 +1,11 @@
+package startpracticetest2exam2.exe23;
+
+public class Test {
+    public static void main(String[] args) {
+        int x=5;
+        while (x<10){
+            System.out.println(x);
+            x++;
+        }
+    }
+}
