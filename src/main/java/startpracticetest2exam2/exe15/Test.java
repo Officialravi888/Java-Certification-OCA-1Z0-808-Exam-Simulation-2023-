a618c42e1b18ba62cf7b96784764740165f10a4a
Officialravi888 <youremail@yourdomain.com>
2023-04-03 10:53:15 +0530

    exe15

---
+++ src/main/java/startpracticetest2exam2/exe15/Test.java
@@ -0,0 +1,14 @@
+package startpracticetest2exam2.exe15;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add(null);
+        list.add(null);
+        list.add(null);
+        System.out.println(list.remove(0) + ":" + list.remove(null));
+    }
+}
