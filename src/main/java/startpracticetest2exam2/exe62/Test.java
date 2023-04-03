ff1d3f013e8b1dad22ce62ea21fb26217ec1e59d
Officialravi888 <youremail@yourdomain.com>
2023-04-03 12:50:33 +0530

    exe62

---
+++ src/main/java/startpracticetest2exam2/exe62/Test.java
@@ -0,0 +1,9 @@
+package startpracticetest2exam2.exe62;
+
+public class Test {
+    public static void main(String[] args) {
+        String s1 = "OcA";
+        String s2 = "oCa";
+        System.out.println(s1.equals(s2));
+    }
+}
