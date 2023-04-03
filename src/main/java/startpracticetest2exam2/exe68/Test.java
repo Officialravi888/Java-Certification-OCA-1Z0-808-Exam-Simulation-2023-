4c32302a8dc6d9ef5d912b6b5c563fe66a2ce759
Officialravi888 <youremail@yourdomain.com>
2023-04-03 12:57:51 +0530

    exe68

---
+++ src/main/java/startpracticetest2exam2/exe68/Test.java
@@ -0,0 +1,6 @@
+//package startpracticetest2exam2.exe68;
+//
+//    public interface Test {
+//        void m1() throws java.io.IOException;
+//    }
+//}
---
+++ src/main/java/startpracticetest2exam2/exe69/Test.java
@@ -0,0 +1,8 @@
+package startpracticetest2exam2.exe69;
+
+public class Test {
+        public static void main(String args[])
+        {
+            System.out.println(args[1]);
+        }
+}
