7d48362d05944267f3ab7fd6c866402c22bec23d
Officialravi888 <youremail@yourdomain.com>
2023-04-03 10:38:45 +0530

    exe9

---
+++ src/main/java/startpracticetest2exam2/exe9/Test.java
@@ -0,0 +1,9 @@
+package startpracticetest2exam2.exe9;
+
+public class Test {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hurrah! I Passed...");
+        sb.delete(0, 100);
+        System.out.println(sb.length());
+    }
+}
