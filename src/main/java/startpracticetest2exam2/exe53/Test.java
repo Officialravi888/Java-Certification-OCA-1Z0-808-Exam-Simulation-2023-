7a06222410716e96caf569d2901c740f42617e2a
Officialravi888 <youremail@yourdomain.com>
2023-04-03 12:39:20 +0530

    exe53

---
+++ src/main/java/startpracticetest2exam2/exe53/Test.java
@@ -0,0 +1,19 @@
+package startpracticetest2exam2.exe53;
+
+    class Parent {
+        public String toString() {
+            return "Inner ";
+        }
+    }
+
+    class Child extends Parent {
+        public String toString() {
+            return super.toString().concat("Peace!");
+        }
+    }
+
+    public class Test {
+        public static void main(String[] args) {
+            System.out.println(new Child());
+        }
+    }
