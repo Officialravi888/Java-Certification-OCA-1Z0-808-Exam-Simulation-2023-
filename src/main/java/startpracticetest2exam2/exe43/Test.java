411945c90a837ab215cfedb865ae0b489cf89408
Officialravi888 <youremail@yourdomain.com>
2023-04-03 12:12:35 +0530

    exe43

---
+++ src/main/java/startpracticetest2exam2/exe43/Test.java
@@ -0,0 +1,11 @@
+package startpracticetest2exam2.exe43;
+
+public class Test {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Java");
+        String s1 = sb.toString();
+        String s2 = "Java";
+
+        System.out.println(s1 == s2);
+    }
+}
