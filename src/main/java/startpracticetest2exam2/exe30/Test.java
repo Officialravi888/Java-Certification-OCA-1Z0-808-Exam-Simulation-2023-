7116a15b10b2a346e380d9cd41190caaace604cd
Officialravi888 <youremail@yourdomain.com>
2023-04-03 11:23:13 +0530

    exe30

---
+++ src/main/java/startpracticetest2exam2/exe30/Test.java
@@ -0,0 +1,8 @@
+package startpracticetest2exam2.exe30;
+
+public class Test {
+    public static void main(String[] args) {
+        String [] arr = new String[7];
+        System.out.println(arr);
+    }
+}
