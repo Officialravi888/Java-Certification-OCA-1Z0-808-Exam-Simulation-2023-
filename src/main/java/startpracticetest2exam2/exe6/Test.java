f7247aef814006cca0963c1c147dcf40f9fd709a
Officialravi888 <youremail@yourdomain.com>
2023-04-03 10:35:06 +0530

    exe7

---
+++ src/main/java/startpracticetest2exam2/exe6/Test.java
@@ -0,0 +1,13 @@
+package startpracticetest2exam2.exe6;
+
+public class Test {
+    public static void main(String[] args) {
+        int grade = 75;
+        if (grade > 60) {
+            System.out.println("Congratulations");
+            System.out.println("You passed");
+        } else {
+            System.out.println("You failed");
+        }
+    }
+}
---
+++ src/main/java/startpracticetest2exam2/exe7/Test.java
@@ -0,0 +1,5 @@
+package startpracticetest2exam2.exe7;
+
+public class Test {
+
+}
