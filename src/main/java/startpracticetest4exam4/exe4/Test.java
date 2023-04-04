57a1f7da5303196ce72b67f5865ac1d39e00d443
Officialravi888 <youremail@yourdomain.com>
2023-04-04 10:10:57 +0530

    exe4

---
+++ src/main/java/startpracticetest4exam4/exe4/Test.java
@@ -0,0 +1,15 @@
+package startpracticetest4exam4.exe4;
+
+public class Test {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder=new StringBuilder();
+        try{
+            for(;;){
+                stringBuilder.append("Cao");
+            }
+        }catch (Exception e){
+            System.out.println("---------");
+        }
+        System.out.println("-----------------------------");
+    }
+}
