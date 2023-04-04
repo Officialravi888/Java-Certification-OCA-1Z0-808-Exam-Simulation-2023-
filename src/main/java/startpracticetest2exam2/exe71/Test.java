3e6b4ff35522b1464c4fc3d817006c2ab1530f59
Officialravi888 <youremail@yourdomain.com>
2023-04-04 09:56:10 +0530

    exe1

---
+++ src/main/java/startpracticetest2exam2/exe71/Test.java
@@ -0,0 +1,22 @@
+//package startpracticetest2exam2.exe71;
+//
+//public class Test {
+//    public static void main(String[] args) {
+//        int score = 60;
+//        switch (score) {
+//            default:
+//                System.out.println("Not a valid score");
+//            case score < 70:
+//                System.out.println("Failed");
+//                break;
+//            case score >= 70:
+//                System.out.println("Passed");
+//                break;
+//        }
+//    }
+//}
+//
+//
+//
+//
+//
---
+++ src/main/java/startpracticetest4exam4/exe1/Test.java
@@ -0,0 +1,8 @@
+package startpracticetest4exam4.exe1;
+
+public class Test {
+    public static void main(String[] args) {
+        Boolean []arr=new Boolean[2];
+        System.out.println(arr[0]+":"+arr[1]);
+    }
+}
