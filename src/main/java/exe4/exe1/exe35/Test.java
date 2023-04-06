243de436393a7cf518eef7a1f6d69bfb9a54df6f
Officialravi888 <youremail@yourdomain.com>
2023-04-06 11:44:13 +0530

    exe35

---
+++ src/main/java/exe4/exe1/exe35/Test.java
@@ -0,0 +1,11 @@
+package exe4.exe1.exe35;
+
+
+public class Test {
+    static Double d1;
+    int x = d1.intValue();
+
+    public static void main(String[] args) {
+        System.out.println("HELLO");
+    }
+}
---
+++ src/main/java/exe4/exe1/exe36/Test.java
@@ -0,0 +1,11 @@
+package exe4.exe1.exe36;
+
+
+public class Test {
+    public static void main(String[] args) {
+        String [] arr = {"abc", "TrUe", "false", null, "FALSE"};
+        for(String s : arr) {
+            System.out.print(Boolean.valueOf(s) ? "T" : "F");
+        }
+    }
+}
