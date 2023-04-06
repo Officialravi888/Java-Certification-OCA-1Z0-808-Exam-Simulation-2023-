f76369b74fcadaccc68875ae83cf1b0086c13245
Officialravi888 <youremail@yourdomain.com>
2023-04-06 11:37:51 +0530

    exe31

---
+++ src/main/java/exe4/exe1/exe31/Test.java
@@ -0,0 +1,12 @@
+package exe4.exe1.exe31;
+
+public class Test {
+    static String msg; //Line 2
+    public static void main(String[] args) {
+        String msg; //Line 4
+        if(args.length > 0) {
+            msg = args[0]; //Line 6
+        }
+        System.out.println(); //Line 8
+    }
+}
