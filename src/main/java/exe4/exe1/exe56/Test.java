f55b609c06417bfa3e54310dc1167a89683500c6
Officialravi888 <youremail@yourdomain.com>
2023-04-06 12:23:42 +0530

    exe56

---
+++ src/main/java/exe4/exe1/exe56/Test.java
@@ -0,0 +1,21 @@
+package exe4.exe1.exe56;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List<String> fruits = new ArrayList<>();
+        fruits.add("apple");
+        fruits.add("orange");
+        fruits.add("grape");
+        fruits.add("mango");
+        fruits.add("banana");
+        fruits.add("grape");
+
+        if(fruits.remove("grape"))
+            fruits.remove("apple");
+
+        System.out.println(fruits);
+    }
+}
