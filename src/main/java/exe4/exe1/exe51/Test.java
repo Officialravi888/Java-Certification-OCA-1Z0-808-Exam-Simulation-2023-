06d1614cc8b6bd5d9cabd55e9ad32f5ba5c0ca1e
Officialravi888 <youremail@yourdomain.com>
2023-04-06 12:10:11 +0530

    exe51

---
+++ src/main/java/exe4/exe1/exe51/Test.java
@@ -0,0 +1,10 @@
+package exe4.exe1.exe51;
+
+import java.time.LocalTime;
+
+public class Test {
+    public static void main(String [] args) {
+        LocalTime time = LocalTime.of(2, 60);
+        System.out.println(time);
+    }
+}
