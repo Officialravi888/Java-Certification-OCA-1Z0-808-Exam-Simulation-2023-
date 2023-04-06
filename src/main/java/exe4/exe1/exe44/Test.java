5f30f0829522610bd027f6454b9d8b90d7904a32
Officialravi888 <youremail@yourdomain.com>
2023-04-06 11:55:58 +0530

    exe44

---
+++ src/main/java/exe4/exe1/exe44/Test.java
@@ -0,0 +1,11 @@
+package exe4.exe1.exe44;
+
+
+public class Test {
+    public static void main(String[] args) {
+        do {
+            System.out.println(100);
+        } while (true);
+
+    }
+}
