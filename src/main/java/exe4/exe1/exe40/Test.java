851bdaeb7f2b5d9295f6504af88043e8da1a4c2c
Officialravi888 <youremail@yourdomain.com>
2023-04-06 11:49:32 +0530

    exe40

---
+++ src/main/java/exe4/exe1/exe40/Test.java
@@ -0,0 +1,20 @@
+package exe4.exe1.exe40;
+
+public class Test {
+    public static void main(String[] args) {
+        /*INSERT*/
+        int var=7;
+        switch(var) {
+            case '7':
+                System.out.println("Lucky no. 7");
+                break;
+            default:
+                System.out.println("DEFAULT");
+        }
+    }
+}
+
+
+
+
+
