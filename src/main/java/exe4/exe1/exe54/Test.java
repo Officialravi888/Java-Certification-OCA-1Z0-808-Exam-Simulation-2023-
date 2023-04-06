51a4e2ff322b57a46178146c9a0db4fcf6be1a27
Officialravi888 <youremail@yourdomain.com>
2023-04-06 12:17:11 +0530

    exe54

---
+++ src/main/java/exe4/exe1/exe54/Test.java
@@ -0,0 +1,10 @@
+package exe4.exe1.exe54;
+
+import java.time.LocalDate;
+
+public class Test {
+    public static void main(String[] args) {
+        LocalDate localDate=LocalDate.of(2068,4,15);
+        System.out.println(localDate.getMonth()+":"+ localDate.getMonthValue());
+    }
+}
