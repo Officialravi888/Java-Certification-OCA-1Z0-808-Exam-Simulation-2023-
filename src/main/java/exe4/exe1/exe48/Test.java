dc64b01d832d1ecf0be337c11904005d4b1299d6
Officialravi888 <youremail@yourdomain.com>
2023-04-06 12:08:35 +0530

    exe50

---
+++ src/main/java/exe4/exe1/exe48/Test.java
@@ -0,0 +1,34 @@
+package exe4.exe1.exe48;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+abstract class Super {
+    public abstract void m1() throws IOException;
+}
+
+class Sub extends Super {
+    @Override
+    public void m1() throws IOException {
+        throw new FileNotFoundException();
+    }
+}
+
+public class Test {
+    public static void main(String[] args) {
+        Super s = new Sub();
+        try {
+            s.m1();
+        } catch (FileNotFoundException e) {
+            System.out.print("X");
+        } catch (IOException e) {
+            System.out.print("Y");
+        } finally {
+            System.out.print("Z");
+        }
+    }
+}
+
+
+
+
---
+++ src/main/java/exe4/exe1/exe50/Test.java
@@ -0,0 +1,9 @@
+package exe4.exe1.exe50;
+
+public class Test {
+    public static void main(String[] args) {
+        int i;
+        for(i=0; i<=2; i++){}
+        System.out.println(i);
+    }
+}
