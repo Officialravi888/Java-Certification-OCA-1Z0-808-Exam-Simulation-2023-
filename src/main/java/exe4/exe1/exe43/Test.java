3f7990474524b30d35165c1be95ca7756289e00b
Officialravi888 <youremail@yourdomain.com>
2023-04-06 11:56:38 +0530

    exe43

---
+++ src/main/java/exe4/exe1/exe43/Test.java
@@ -0,0 +1,16 @@
+package exe4.exe1.exe43;
+
+public class Test {
+    public static void main(String[] args) {
+        final boolean flag;
+        flag = false;
+        while(flag) {
+            System.out.println("Good Morning!");
+        }
+    }
+}
+
+
+
+
+
