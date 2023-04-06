588119d708e05da6d09833d9417cc4fd65091b98
Officialravi888 <youremail@yourdomain.com>
2023-04-06 12:57:02 +0530

    exe68

---
+++ src/main/java/exe4/exe1/exe68/Counter.java
@@ -0,0 +1,17 @@
+package exe4.exe1.exe68;
+
+public class Counter {
+    int count;
+
+    private static void increment(Counter counter) {
+        counter.count++;
+    }
+
+    public static void main(String [] args) {
+        Counter c1 = new Counter();
+        Counter c2 = c1;
+        Counter c3 = null;
+        c2.count = 1000;
+        increment(c2);
+    }
+}
