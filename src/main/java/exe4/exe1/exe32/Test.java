0f16e0cf105ba2a8eb7f724eaec7b2845551c99a
Officialravi888 <youremail@yourdomain.com>
2023-04-06 11:39:43 +0530

    exe32

---
+++ src/main/java/exe4/exe1/exe32/Test.java
@@ -0,0 +1,10 @@
+package exe4.exe1.exe32;
+
+public class Test {
+    private static int [] arr;
+    public static void main(String [] args) {
+        if(arr.length > 0 && arr != null) {
+            System.out.println(arr[0]);
+        }
+    }
+}
