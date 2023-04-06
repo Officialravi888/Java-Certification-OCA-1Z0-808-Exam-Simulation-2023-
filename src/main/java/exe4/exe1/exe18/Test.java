291d8cc6159350a31a550cfdce88e64e53a66cba
Officialravi888 <youremail@yourdomain.com>
2023-04-06 11:07:14 +0530

    exe18

---
+++ src/main/java/exe4/exe1/exe18/Test.java
@@ -0,0 +1,8 @@
+package exe4.exe1.exe18;
+
+public class Test {
+    public static void main(String[] args) {
+        Boolean [] arr = new Boolean[2];
+        System.out.println(arr[0] + ":" + arr[1]);
+    }
+}
