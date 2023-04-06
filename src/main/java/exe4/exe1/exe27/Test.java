50e2f628ed4f6d35cfd405a23fdfa4ebd7229a77
Officialravi888 <youremail@yourdomain.com>
2023-04-06 11:29:00 +0530

    exe27

---
+++ src/main/java/exe4/exe1/exe27/Test.java
@@ -0,0 +1,9 @@
+package exe4.exe1.exe27;
+
+
+public class Test {
+    public static void main(String [] args) {
+        int a = 3;
+        System.out.println(a++ == 3 || --a == 3 && --a == 3);
+    }
+}
