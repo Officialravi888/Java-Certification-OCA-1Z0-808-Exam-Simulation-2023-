236a8129ea19c62fbb2d15fc93d0b2866d7be634
Officialravi888 <youremail@yourdomain.com>
2023-04-06 11:58:21 +0530

    exe45

---
+++ src/main/java/exe4/exe1/exe45/DivModTest.java
@@ -0,0 +1,9 @@
+package exe4.exe1.exe45;
+
+
+public class DivModTest {
+    public static void main(String[] args) {
+        System.out.println( 23 / 2.0 );
+        System.out.println( 23 % 2.0 );
+    }
+}
