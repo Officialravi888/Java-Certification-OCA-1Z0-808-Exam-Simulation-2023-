cfe5ca2f5e3e40b10982f7a9b7da5d5ae7cff8cf
Officialravi888 <youremail@yourdomain.com>
2023-04-06 11:40:36 +0530

    exe33

---
+++ src/main/java/exe4/exe1/exe33/Test.java
@@ -0,0 +1,8 @@
+package exe4.exe1.exe33;
+
+public class Test {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        System.out.println(sb.append("").append("").append("").length());
+    }
+}
