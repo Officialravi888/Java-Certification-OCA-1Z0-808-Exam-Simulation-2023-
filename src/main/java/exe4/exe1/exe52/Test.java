6be501407c968757d469ae12f1924ee77c9767aa
Officialravi888 <youremail@yourdomain.com>
2023-04-06 12:11:55 +0530

    exe52

---
+++ src/main/java/exe4/exe1/exe52/Test.java
@@ -0,0 +1,14 @@
+package exe4.exe1.exe52;
+
+public class Test {
+    public static void main(String[] args) {
+        String fName = "James";
+        String lName = "Gosling";
+        System.out.println(fName = lName);
+    }
+}
+
+
+
+
+
