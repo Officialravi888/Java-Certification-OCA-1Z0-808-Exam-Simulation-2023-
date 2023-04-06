5d735e2b37975d567b17d0bddb14e01e515d3cef
Officialravi888 <youremail@yourdomain.com>
2023-04-06 11:10:59 +0530

    exe19

---
+++ src/main/java/exe4/exe1/exe19/Test.java
@@ -0,0 +1,11 @@
+package exe4.exe1.exe19;
+
+
+import java.time.LocalDate;
+
+public class Test {
+    public static void main(String [] args) {
+        LocalDate obj = LocalDate.now();
+        System.out.println(obj.getMonth());
+    }
+}
