68de720d0d85d65d6e8edd5ebad89c3443e7d0c7
Officialravi888 <youremail@yourdomain.com>
2023-04-07 13:02:48 +0530

    exe13

---
+++ src/main/java/exe5/exe13/Test.java
@@ -0,0 +1,10 @@
+package exe5.exe13;
+
+public class Test {
+    public static void main(String[] args) {
+        boolean t1="Java"=="Java".replace('J','J');
+        boolean t2="Java"=="Java".replace("J","J");
+        System.out.println(t1&t2);
+
+    }
+}
