b49155293f816403710f0f25cbe2d368d9bf3a17
Officialravi888 <youremail@yourdomain.com>
2023-04-07 13:39:23 +0530

    exe16

---
+++ src/main/java/exe5/exe16/Test.java
@@ -0,0 +1,16 @@
+//package exe5.exe16;
+//
+//public class Test {
+//    String name;
+//    Test(String name){
+//        this.name=name;
+//    }
+//    public String toString(){
+//        return "Plant:"+name;
+//    }
+//}
+//public class Creator{
+//    public static boolean create(){
+//        return new Plant("Earth");
+//    }
+//}
---
+++ src/main/java/exe5/exe16/TestCreator.java
@@ -0,0 +1,7 @@
+package exe5.exe16;
+
+public class TestCreator {
+    public static void main(String[] args) {
+        System.out.println(Creator.create());
+    }
+}
