0c78aef1afcdbbebcca3fa1ac2858260110d1ba2
Officialravi888 <youremail@yourdomain.com>
2023-04-07 13:13:40 +0530

    exe14

---
+++ src/main/java/exe5/exe14/Test.java
@@ -0,0 +1,22 @@
+//package exe5.exe14;
+//
+//import static javax.management.Query.div;
+//
+//public class Test {
+//    private static int add(int i, int j){
+//        try {
+//            System.out.println((i/j));
+//        }catch (ArithmeticException e){
+//            Exception ex=new Exception();
+////            throw ex;
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        try{
+//            div(5,0);
+//        }catch (Exception e){
+//            System.out.println("END");
+//        }
+//    }
+//}
