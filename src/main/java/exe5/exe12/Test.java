de989be8bf24539a3d73c8e63e53cf880856ec16
Officialravi888 <youremail@yourdomain.com>
2023-04-07 12:59:18 +0530

    exe12

---
+++ src/main/java/exe5/exe12/Test.java
@@ -0,0 +1,8 @@
+package exe5.exe12;
+
+public class Test {
+    public static void main(String[] args) {
+        double x=7,y=200;
+        System.out.println(String.valueOf(x+y).length());
+    }
+}
