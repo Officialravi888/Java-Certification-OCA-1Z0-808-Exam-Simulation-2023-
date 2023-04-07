05eb96b31c86ab8cbf9352da593e0159c0ddda4e
Officialravi888 <youremail@yourdomain.com>
2023-04-07 12:42:48 +0530

    exe10

--- src/main/java/exe5/exe10/Test.java
+++ src/main/java/exe5/exe10/Test.java
@@ -1,4 +1,14 @@
 package exe5.exe10;
 
+interface Counter{
+    int Count=10;
+
+}
 public class Test {
+    public static void main(String[] args) {
+        Counter[]arr=new Counter[2];
+        for (Counter c:arr){
+            System.out.println(c);
+        }
+    }
 }
