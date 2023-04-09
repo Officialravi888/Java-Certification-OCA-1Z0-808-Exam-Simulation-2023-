21965147935dc79689e2e38c13c57b01bec0e146
Officialravi888 <youremail@yourdomain.com>
2023-04-09 18:15:49 +0530

    exe10

---
+++ src/main/java/startpracticetest6exam6/exe1/exe10/Test.java
@@ -0,0 +1,9 @@
+package startpracticetest6exam6.exe1.exe10;
+
+public class Test {
+    public static void main(String[] args) {
+        String[]arr={"1st","2st"};
+        String s="Ravi";
+       // System.out.println(arr[place.indexOf("a",2)]);
+    }
+}
