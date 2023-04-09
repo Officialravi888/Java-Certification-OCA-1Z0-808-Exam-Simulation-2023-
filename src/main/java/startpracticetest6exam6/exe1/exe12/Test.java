ac733cc6088948d046a671f623f1d9dfa55b6408
Officialravi888 <youremail@yourdomain.com>
2023-04-09 18:26:52 +0530

    exe12

---
+++ src/main/java/startpracticetest6exam6/exe1/exe12/Test.java
@@ -0,0 +1,9 @@
+package startpracticetest6exam6.exe1.exe12;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(new RuntimeException());
+        System.out.println(new RuntimeException("Hello"));
+        System.out.println(new RuntimeException(new RuntimeException("Hello")));
+    }
+}
