478c706521eb8eda99e68e841b61ec0787107680
Officialravi888 <youremail@yourdomain.com>
2023-04-09 18:28:35 +0530

    exe13

---
+++ src/main/java/startpracticetest6exam6/exe1/exe13/Test.java
@@ -0,0 +1,8 @@
+package startpracticetest6exam6.exe1.exe13;
+
+public class Test {
+    public static void main(String[] args) {
+        String str = "Hello";
+        System.out.println(str.replace("A","v"));
+    }
+}
