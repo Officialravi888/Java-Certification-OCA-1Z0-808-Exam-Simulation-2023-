0b751ada60bfc83ef83d749a6afcd69efc1bc1cc
Officialravi888 <youremail@yourdomain.com>
2023-04-09 17:30:58 +0530

    exe2

---
+++ src/main/java/startpracticetest6exam6/exe1/exe2/Test.java
@@ -0,0 +1,9 @@
+package startpracticetest6exam6.exe1.exe2;
+
+public class Test {
+    public static void main(String[] args) {
+        boolean status=true;
+        System.out.println(status=false);
+        System.out.println(status);
+    }
+}
