98908dde7926fcb3373a881a9e0adc74acef2954
Officialravi888 <youremail@yourdomain.com>
2023-04-10 16:11:56 +0530

    exe27

---
+++ src/main/java/startpracticetest6exam6/exe1/exe27/Test.java
@@ -0,0 +1,10 @@
+package startpracticetest6exam6.exe1.exe27;
+
+public class Test {
+    public static void main(String[] args) {
+        int x=10;
+        if(true)
+        System.out.println(x);
+        System.out.println("Hello");
+    }
+}
