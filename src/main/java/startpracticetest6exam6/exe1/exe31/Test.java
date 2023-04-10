2a27cc315396914c5be16d290f9b4ff36810e937
Officialravi888 <youremail@yourdomain.com>
2023-04-10 16:39:42 +0530

    exe33

---
+++ src/main/java/startpracticetest6exam6/exe1/exe31/Test.java
@@ -0,0 +1,9 @@
+package startpracticetest6exam6.exe1.exe31;
+
+public class Test {
+    public static void main(String[] args) {
+        int a=0;
+        int b=1;
+        System.out.println(a+b);
+    }
+}
---
+++ src/main/java/startpracticetest6exam6/exe1/exe33/Test.java
@@ -0,0 +1,9 @@
+package startpracticetest6exam6.exe1.exe33;
+
+public class Test {
+    public static void main(String[] args) {
+        boolean statuc = true;
+        System.out.println(statuc=true);
+        System.out.println(statuc);
+    }
+}
